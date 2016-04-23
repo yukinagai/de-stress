628b9163250135cd6625e4fdb453f88880347895
Takeshi Kubo <t-kubo@kddilabs.jp>
2016-04-23 13:56:23 +0900

    add sending function from wear to smartphone

--- VitalSense/wear/src/main/java/jp/kddilabs/vitalsense/MainActivity.java
+++ VitalSense/wear/src/main/java/jp/kddilabs/vitalsense/MainActivity.java
@@ -1,6 +1,7 @@
 package jp.kddilabs.vitalsense;
 
 import android.app.Activity;
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -10,13 +11,24 @@ import android.support.wearable.view.WatchViewStub;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends Activity {
+    private final String TAG = "Wear";
     private TextView mTextView;
     private SensorManager mSensorManager;
     private Sensor mHeartRateSensor;
+    private Activity mActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mActivity = (Activity) this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
@@ -46,7 +58,9 @@ public class MainActivity extends Activity {
     final SensorEventListener mHeartListener = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent event) {
-            mTextView.setText(String.valueOf(event.values[0]));
+            String valStr = String.valueOf(event.values[0]);
+            mTextView.setText(valStr);
+            sendMessage(mActivity, valStr);
         }
 
         @Override
@@ -55,4 +69,23 @@ public class MainActivity extends Activity {
         }
     };
 
-}
+    public void sendMessage(final Context context, final String valStr) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                GoogleApiClient client = new GoogleApiClient.Builder(context)
+                        .addApi(Wearable.API)
+                        .build();
+                client.blockingConnect(100, TimeUnit.MILLISECONDS);
+                NodeApi.GetConnectedNodesResult result =
+                        Wearable.NodeApi.getConnectedNodes(client).await();
+                List<Node> nodes = result.getNodes();
+                if (nodes.size() > 0) {
+                    client.blockingConnect(100, TimeUnit.MILLISECONDS);
+                    Wearable.MessageApi.sendMessage(client, nodes.get(0).getId(), valStr, null);
+                    client.disconnect();
+                }
+                client.disconnect();
+            }
+        }).start();
+    }}
