35c169acb2571bf8872e0e960aab7090af2911ac
Takeshi Kubo <t-kubo@kddilabs.jp>
2016-04-24 00:01:23 +0900

    add onDestory on wear

--- VitalSense/wear/src/main/java/jp/kddilabs/vitalsense/MainActivity.java
+++ VitalSense/wear/src/main/java/jp/kddilabs/vitalsense/MainActivity.java
@@ -49,6 +49,13 @@ public class MainActivity extends Activity {
             }
         });
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mSensorManager.unregisterListener(mHeartListener);
+    }
+
     /*
     @Override
     public void onSensorChanged(SensorEvent event) {
