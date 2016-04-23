e3c0ac96dff376d84a29c973920ce8ccd0381bfe
Hiroshi Hanano <16c16c@gmail.com>
2016-04-23 22:05:08 +0900

    バイタルとお風呂のグラフを表示できる様にした

--- VitalNebula/app/controllers/main_controller.rb
+++ VitalNebula/app/controllers/main_controller.rb
@@ -1,5 +1,6 @@
 class MainController < ApplicationController
   def index
-    @heartrates = Vital.where(device: "moto360").where("value <> 0").order("created_at asc")
+    @heartrates = Vital.where(device: "moto360", sensor: "heartrate").where("value <> 0").order("created_at asc")
+    @temperatures = Bath.where(sensor: "temperature").where("value <> 0").order("created_at asc")
   end
 end
